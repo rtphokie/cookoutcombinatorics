8f4c5cd030479e30827e28c0f4b615fa5f2d7a7c
trice <rtphokie@gmail.com>
2024-06-23 18:59:15 -0400

    adding possible combinations listing

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # cookoutcombinatorics
 Calculating the number of different ways you can order a tray (or jr. tray) from Cookout restaurants in the mid-Atlantic states.
+
+Cookout trays consist of an entree (20 to choose from), two sides (136 total combinations), and a beverage (56 including 45 milkshake flavors).
+
+This makes for a possible 152,320 possible 
+
+entress: 20
+sides: 16 for 136 combinations
+beverages: 56
+total: 152,320
+total summer: 155,040
+total winter: 157,760
+
+burger toppings: 262,143
+dog toppings: 3
+total with toppings: 11,979,076,928
+highest calorie orders (2639 calories):
+ -
+chicken strip sandwich, double up ranch chicken wrap, and a Cherry Cheesecake shake (2639 calories)
+ -chicken strip sandwich, double up ranch chicken wrap, and a Blueberry Cheesecake shake (2639 calories)
+ -chicken strip sandwich, double up ranch chicken wrap, and a Carmel Cheesecake shake (2639 calories)
+ -chicken strip sandwich, double up ranch chicken wrap, and a Strawberry Cheesecake shake (2639 calories)
+ -chicken strip sandwich, double up ranch chicken wrap, and a Choc Chip Cheesecake shake (2639 calories)
+lowest calorie orders (420 calories):
+ -
+chicken quesadilla, double up slaw, and a bottled water (420 calories)
+ -chicken quesadilla, double up slaw, and a large Diet Coke (420 calories)
+
--- cookoutchoices.py
+++ cookoutchoices.py
@@ -29,24 +29,66 @@ def cookout_combinatorics():
     sides = build_dictionaries([data['side']])
     side_combinations = list(combinations(sides.keys(), r=2))
     side_combinations.extend([(side, side) for side in sides.keys()])
-    beverages = build_dictionaries(data['beverage'].values())
-    beverages.update(build_dictionaries(data['shake seasonal'].values()))
+    beverages_summer = build_dictionaries([data['beverage'], data['shake'], data['shake seasonal']['summer']])
+    beverages_winter = build_dictionaries( [data['beverage'], data['shake'], data['shake seasonal']['winter']])
+    beverages = build_dictionaries( [data['beverage'], data['shake']])
+
+    burger_options, dog_options = get_topping_combinations()
+
 
     total_combinations = len(entrees) * len(side_combinations) * len(beverages)
-    total_combinations_with_toppings = (len(entrees) + (5 * 262142)) * len(side_combinations) * len(beverages)
-
-    total_combinations = len(entrees) * len(side_combinations) * len(beverages)
-    # 5 sandwiches: small, regular, and big double burgers as well as regular chicken or chicken strip sandwiche0ww:
-    total_combinations_with_toppings = (len(entrees) + (5 * 262142)) * len(side_combinations) * len(beverages)
+    total_combinations_summer = len(entrees) * len(side_combinations) * len(beverages_summer)
+    total_combinations_winter = len(entrees) * len(side_combinations) * len(beverages_winter)
+    # 6 sandwiches: small, regular, and big double burgers as well as regular, spicy and chicken strip sandwiche0ww:
+    total_combinations_with_toppings = (len(entrees)-1 + (6 * len(burger_options))  + (2*len(dog_options))) * len(side_combinations) * len(beverages)
+    # removing 1 from the entrees for the cajun chicken sandwich with is effetively created as one of the topping combinations.
+    # keeping all other entreees to serve as plain options.
     print(f"entress: {len(entrees):,}")
     print(f"sides: {len(sides):,} for {len(side_combinations):,} combinations")
     print(f"beverages: {len(beverages):,}")
     print(f"total: {total_combinations:,}")
+    print(f"total summer: {total_combinations_summer:,}")
+    print(f"total winter: {total_combinations_winter:,}")
+
+    print()
+    print(f"burger toppings: {len(burger_options):,}")
+    print(f"dog toppings: {len(dog_options):,}")
     print(f"total with toppings: {total_combinations_with_toppings:,}")
+
+    orders={}
+    for entree in entrees.keys():
+        for side in side_combinations:
+            for beverage in beverages.keys():
+                calories=entrees[entree]['calories']+sides[side[0]]['calories']+sides[side[1]]['calories']+beverages[beverage]['calories']
+                if side[0] == side[1]:
+                    order = f"{entree}, double up {side[0]}, and a {beverage} ({calories} calories)"
+                else:
+                    order = f"{entree}, with {side[0]} and {side[1]}, and a {beverage} ({calories} calories)"
+                orders[order]=calories
+    with open('order_combinations.text', 'w') as outfile:
+        outfile.write("\n".join(orders.keys()))
+    highest_calories = max(orders.values())
+    lowest_calories = min(orders.values())
+    highest_calorie_orders = [key for key in orders if orders[key] ==highest_calories]
+    lowest_calorie_orders = [key for key in orders if orders[key] ==lowest_calories]
+    print(f"highest calorie orders ({highest_calories} calories):\n -")
+    print("\n -".join(highest_calorie_orders))
+    print(f"lowest calorie orders ({lowest_calories} calories):\n -")
+    print("\n -".join(lowest_calorie_orders))
+
     return total_combinations, total_combinations_with_toppings
 
 
+
 def get_topping_combinations():
+    """
+    Returns all possible combinations of burger toppings and hot dog toppings.
+    From just a single topping to all possible toppings.  Note: plain is not included here
+
+    Returns:
+        burger_options (list): A list of all possible combinations of burger and chicken sandwich toppings
+        dog_options (list): A list of all possible combinations of hot dog toppings.
+    """
     data = get_menu()
 
     choices = list(data['burger toppings'].keys())
@@ -56,7 +98,7 @@ def get_topping_combinations():
         burger_options += list(perm)
 
     dog_options = []
-    hotdogtoppings = ['ketchup', 'mustard', 'slaw', 'onion', 'chili']
+    hotdogtoppings = list(data['dog toppings'].keys())
     for r in range(1, len(hotdogtoppings) + 1):
         perm = combinations(hotdogtoppings, r=r)
         dog_options += list(perm)
@@ -66,8 +108,10 @@ def get_topping_combinations():
 class MyTestCase(unittest.TestCase):
     def test_topings(self):
         burger_options, dog_options = get_topping_combinations()
-        print(f"burger and chicken toppings: {len(burger_options):,}")
-        print(f"hot dog toppings: {len(dog_options):,}")
+        # print(f"burger and chicken toppings: {len(burger_options):,}")
+        # print(f"hot dog toppings: {len(dog_options):,}")
+        self.assertEqual(262143, len(burger_options))
+        self.assertEqual(3, len(dog_options))
 
     def test_beverages(self):
         data = get_menu()
@@ -83,8 +127,10 @@ class MyTestCase(unittest.TestCase):
     def test_sides(self):
         data = get_menu()
         sides = build_dictionaries([data['side']])
-        self.assertEqual(15, len(sides))
+        self.assertEqual(16, len(sides))
 
+    def test_main(self):
+        cookout_combinatorics()
 
 if __name__ == '__main__':
     cookout_combinatorics()
